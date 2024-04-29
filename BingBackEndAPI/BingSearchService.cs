aee6a48b6f1ba2157dcd2bde82ae7b310a4fef16
rajesh dharmavarapu <dharmavarapurajesh@outlook.com>
2024-04-29 10:46:19 +0530

    to display Bing App search

---
+++ BingBackEndAPI/BingSearchService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class BingSearchService
+{
+    private readonly HttpClient _httpClient;
+
+    public BingSearchService(HttpClient httpClient)
+    {
+        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+    }
+
+    public async Task<string> SearchAsync(string query)
+    {
+        try
+        {
+            var url = $"https://api.bing.microsoft.com/v7.0/search?q={Uri.EscapeDataString(query)}";
+            var apiKey = "7f5a827efeaf4791b370b23fe720179c";
+            _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+            var response = await _httpClient.GetStringAsync(url);
+            return response;
+        }
+        catch(Exception ex)
+        {
+            throw ex;
+        }
+    }
+}
---
+++ BingBackEndAPI/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SearchController : ControllerBase
+{
+    private readonly BingSearchService _bingSearchService;
+
+    public SearchController(BingSearchService bingSearchService)
+    {
+        _bingSearchService = bingSearchService;
+    }
+
+    [AllowAnonymous]
+
+    [HttpGet]
+    public async Task<IActionResult> Get(string query)
+    {
+        var result = await _bingSearchService.SearchAsync(query);
+        return Ok(result);
+    }
+}
